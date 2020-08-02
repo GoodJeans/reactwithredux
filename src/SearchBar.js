580f52c908b7fcdb1b5af08b72b515ad06f31dd8
GoodJeans <limguanji@gmail.com>
2020-08-03 01:16:15 +0800

    update

---
+++ src/SearchBar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+
+class SearchBar extends React.Component{
+    
+    onInputChange(event){
+        console.log(event.target.value)
+    }
+
+    state = { term : ''};
+
+    onFormSubmit= event =>{
+        event.preventDefault();
+        // console.log(this.state.term);
+        this.props.onSubmit(this.state.term);
+    }
+
+    render(){
+        return(
+            <div className="ui segment">
+                <form onSubmit={this.onFormSubmit} className="ui form">
+                    <div className="field">
+                        <label>Image Search</label>
+                        <input type="text" 
+                         onClick={this.onInputClick}
+                         onChange={(e) => this.setState({term: e.target.value.toUpperCase() })}
+                         value={this.state.term}
+                         />
+                    </div>
+                </form>
+            </div>
+        )
+    }
+
+}
+
+export default SearchBar;
