f2f252a5fac83065e881fd391bde5206c45675b7
GoodJeans <limguanji@gmail.com>
2020-08-03 17:44:56 +0800

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,20 @@
 import React from 'react';
-import SearchBar from '../SearchBar';
-import unsplash from '../api/unsplash';
-import ImageList from './ImageList';
-class App extends React.Component {
-  state = { images: [] };
-  constructor(props){
-    super(props);
-  }
+import SongList from './SongList';
+import SongDetail from './SongDetail';
 
-
-  onSearchSubmit= async(term)=>{
-    const response = await  unsplash.get('/search/photos',{
-    params:{query: term},
-  
-    })
-
-      // console.log("response is.."+response.data.results);
-      this.setState({images: response.data.results });
-        }
-
-  render() {
-    return (
-      <div className="ui container" style={{ marginTop: '10px' }}>
-        <SearchBar onSubmit={this.onSearchSubmit} />
-        <ImageList images={this.state.images}/>
-              </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="ui container grid">
+      <div className="ui row">
+        <div className="column eight wide">
+          <SongList />
+        </div>
+        <div className="column eight wide">
+          <SongDetail />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
